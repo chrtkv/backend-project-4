b1f3504fd5cd5b2536487f893804879b37fdb161
Kirill Chertkov <chertkov.k@gmail.com>
2023-09-03 23:05:11 +0300

    add hooks to tests

--- __tests__/pageLoader.test.js
+++ __tests__/pageLoader.test.js
@@ -1,15 +1,24 @@
+import fsp from 'fs/promises';
 import nock from 'nock';
-import { readFile } from 'fs/promises';
+import os from 'os';
+import path from 'path';
+
 import pageLoader from '../src/pageLoader.js';
+
+let outputDir;
+beforeEach(async () => {
+  outputDir = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+});
+afterEach(async () => fsp.rmdir(outputDir, { recursive: true }));
 
 nock('https://ru.hexlet.io')
   .get('/courses')
-  .reply(200, { message: 'Hexlet Courses' });
+  .reply(200, 'Hexlet Courses');
 
 describe('pageLoader', () => {
   it('should save data to the file', async () => {
-    const resultPath = await pageLoader('https://ru.hexlet.io/courses');
-    const result = await readFile(resultPath, 'utf-8');
+    const resultPath = await pageLoader('https://ru.hexlet.io/courses', outputDir);
+    const result = await fsp.readFile(resultPath, 'utf-8');
 
     expect(result).toEqual('Hexlet Courses');
   });
