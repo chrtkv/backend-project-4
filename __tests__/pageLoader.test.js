98a46c728b65c7c661ccbab20af7839f816594f9
Kirill Chertkov <chertkov.k@gmail.com>
2023-09-09 01:43:51 +0300

    add tests

--- __tests__/pageLoader.test.js
+++ __tests__/pageLoader.test.js
@@ -1,11 +1,13 @@
 import fsp from 'fs/promises';
 import nock from 'nock';
-import os from 'os';
 import path from 'path';
+import os from 'os';
 import { fileURLToPath } from 'url';
+import prettier from 'prettier';
 
-import pageLoader from '../src/pageLoader.js';
-import { composeLocalNames, downloadImages, replaceImgSrc } from '../src/utils.js';
+import {
+  downloadHtml, downloadImages, replaceImgSrcs,
+} from '../src/utils.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -24,38 +26,54 @@ beforeEach(async () => {
 afterEach(async () => fsp.rmdir(outputDir, { recursive: true }));
 
 describe('utils', () => {
-  it('should save data to the file', async () => {
+  it('should download data', async () => {
     const expectedResult = await getFixtureContent('original.html');
-
     nock('https://ru.hexlet.io')
       .get('/courses')
       .reply(200, expectedResult);
 
-    const resultPath = await pageLoader(url, outputDir);
+    const resultPath = await downloadHtml(url, outputDir);
     const result = await fsp.readFile(resultPath, 'utf-8');
 
     expect(result).toEqual(expectedResult);
   });
 
-  it('should download images', async () => {
-    const originalHtml = await getFixtureContent('original.html');
+  it('should have correct name', async () => {
+    nock('https://ru.hexlet.io')
+      .get('/courses')
+      .reply(200);
 
+    const resultPath = await downloadHtml(url, outputDir);
+    const expectedResult = path.resolve(outputDir, 'ru-hexlet-io-courses.html');
+
+    expect(resultPath).toEqual(expectedResult);
+  });
+
+  it('should download images', async () => {
     nock('https://ru.hexlet.io')
       .get('/assets/professions/nodejs.png')
       .replyWithFile(200, path.resolve(fixturesPath, 'nodejs.png'));
 
-    const [downloadPath] = await downloadImages(originalHtml, outputDir);
-    const result = await fsp.readFile(downloadPath, 'utf-8');
+    const originalHtmlPath = path.resolve(fixturesPath, 'original.html');
+
+    const [{ newSrc: downloadPath }] = await downloadImages(url, outputDir, originalHtmlPath);
+    const resultFilePath = path.resolve(outputDir, downloadPath);
+    await fsp.access(resultFilePath, fsp.constants.F_OK);
+    const result = await fsp.readFile(resultFilePath, 'utf-8'); // норм ли для картинки?
     const expectedResult = await getFixtureContent('nodejs.png');
 
     expect(result).toEqual(expectedResult);
   });
 
   it('should replace links', async () => {
-    const originalHtml = await getFixtureContent('original.html');
-    const expectedResult = await getFixtureContent('local.html');
+    nock('https://ru.hexlet.io')
+      .get('/assets/professions/nodejs.png')
+      .replyWithFile(200, path.resolve(fixturesPath, 'nodejs.png'));
 
-    const result = await replaceImgSrc(originalHtml, url);
+    const originalHtmlPath = path.resolve(fixturesPath, 'original.html');
+    const imgSrcs = await downloadImages(url, outputDir, originalHtmlPath);
+    const result = prettier.format(await replaceImgSrcs(originalHtmlPath, imgSrcs), { parser: 'html' });
+    const expectedResult = prettier.format(await getFixtureContent('local.html'), { parser: 'html' });
 
     expect(result).toEqual(expectedResult);
   });
